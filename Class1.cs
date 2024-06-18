08d66878ed359773648210235c62f8502a463d02
Natalia Bała <natalia.bala@microsoft.wsei.edu.pl>
2024-06-18 20:10:28 +0200

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PlayerTrackingLibrary
+{
+    public class Location
+    {
+        public DateTime Time { get; }
+        public int X { get; }
+        public int Y { get; }
+
+        public Location(int x, int y)
+        {
+            Time = DateTime.Now;
+            X = x;
+            Y = y;
+        }
+    }
+}
